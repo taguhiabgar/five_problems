97fd3ee5e9ffe063173f175b0a1fec2f07db048b
Taguhi Abgar <taguhiabgar@Taguhis-MacBook-Pro.local>
2017-09-01 17:34:11 +0400

    Initial Commit

---
+++ FiveProblems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FiveProblems
+//
+//  Created by Taguhi Abgar on 9/1/17.
+//  Copyright © 2017 Taguhi Abgar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
